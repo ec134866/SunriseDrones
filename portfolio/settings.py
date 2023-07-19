c2a3b7a2fbee21e6420e35eeb60e160c56807133
ec134866 <evan@thecarters.com>
2023-07-19 11:19:21 -0600

    goodupdate

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -109,6 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 # https://testdriven.io/blog/multiple-languages-in-django/
+# https://docs.djangoproject.com/en/dev/topics/i18n/translation/
 
 LANGUAGE_CODE = 'en'
 LANGUAGES = (
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -21,6 +21,6 @@ from django.utils.translation import gettext_lazy as _
 
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
-    path('rosetta/', include('rosetta.urls')),
+    path('rosetta/', include('rosetta.urls'), name='lang'),
     path('', include('portfolioApp.urls') )
 )
