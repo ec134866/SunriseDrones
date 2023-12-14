10b6a22c96769914aebe84fbfb88b1b767199863
ec134866 <evan@thecarters.com>
2023-12-14 12:01:37 -0700

    fixurl2

--- portfolioApp/urls.py
+++ portfolioApp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('sideprojects/', sideprojectPageView, name = "sideprojects"),
     path('sideprojects/3D/', threeDPageView, name = "threeD"),
     path('sideprojects/translation/', translationPageView, name = "translation"),
-    path('sideprojects/theOceanCleanup/', oceanCleanupPageView, name = "oceanCleanup"),
+    path('theOceanCleanup/', oceanCleanupPageView, name = "oceanCleanup"),
     path('login/', loginPageView, name='login'),
     path('character/', characterView, name='character'),
 
