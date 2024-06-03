a9f0802e22a7de6b79e6b7f031e5c3ddf7ed8af7
ec134866 <evan@thecarters.com>
2024-06-03 17:45:43 -0400

    test1

--- portfolioApp/urls.py
+++ portfolioApp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import indexPageView, analystPageView, dunkerPageView, sideprojectPageView, threeDPageView, loginPageView, characterView, translationPageView, oceanCleanupPageView
+from .views import indexPageView, analystPageView, dunkerPageView, sideprojectPageView, threeDPageView, loginPageView, characterView, translationPageView, oceanCleanupPageView, get_link_token, exchange_public_token, get_balances
 
 urlpatterns = [
     path('', indexPageView, name = "index"),
@@ -11,5 +11,8 @@ urlpatterns = [
     path('theOceanCleanup/', oceanCleanupPageView, name = "oceanCleanup"),
     path('login/', loginPageView, name='login'),
     path('mascot/', characterView, name='character'),
+    path('get_link_token/', get_link_token, name='get_link_token'),
+    path('exchange_public_token/', exchange_public_token, name='exchange_public_token'),
+    path('balances/', get_balances, name='get_balances')
 
 ]
--- portfolioApp/views.py
+++ portfolioApp/views.py
@@ -1,8 +1,42 @@
+import json
+from django.conf import settings
+from plaid import Client
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from django.http import HttpResponse, HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseForbidden, JsonResponse
 from django.contrib.auth.decorators import login_required
+
+
+client = Client(client_id=settings.PLAID_CLIENT_ID,
+                secret=settings.PLAID_SECRET,
+                environment=settings.PLAID_ENV)
+
+def get_link_token(request):
+    response = client.LinkToken.create({
+        'user': {
+            'client_user_id': 'unique_user_id',
+        },
+        'client_name': 'Your App Name',
+        'products': ['auth'],
+        'country_codes': ['US'],
+        'language': 'en',
+    })
+    return JsonResponse(response)
+
+def exchange_public_token(request):
+    public_token = request.POST.get('public_token')
+    exchange_response = client.Item.public_token.exchange(public_token)
+    access_token = exchange_response['access_token']
+    request.session['access_token'] = access_token
+    return HttpResponse(status=200)
+
+def get_balances(request):
+    access_token = request.session['access_token']
+    balance_response = client.Accounts.balance.get(access_token)
+    accounts = balance_response['accounts']
+    return render(request, 'balances.html', {'accounts': accounts})
+
 
 # Create your views here.
 
