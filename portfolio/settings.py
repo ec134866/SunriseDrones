4177b8057d8a6305bd50adb5d0b46e1cb63def0c
ec134866 <evan@thecarters.com>
2024-09-10 16:26:45 -0400

    adding database connection

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -84,8 +84,11 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'SunriseDrones$default',
+        'USER': 'SunriseDrones',
+        'PASSWORD': 'weakPassword',
+        'HOST': 'SunriseDrones.mysql.pythonanywhere-services.com',
     }
 }
 
