80f90dd6220e95132ad2c5f86a2948371dd6d176
ec134866 <evan@thecarters.com>
2024-09-10 12:50:39 -0400

    adding site to allowed_hosts

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -30,7 +30,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
-ALLOWED_HOSTS = ['127.0.0.1', 'evancarter.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'sunrisedrones.pythonanywhere.com']
 
 
 # Application definition
