4a4979b43a0a74cc71f0f57f3b2fec348c301018
ec134866 <evan@thecarters.com>
2024-09-11 16:46:45 -0400

    changes to base

--- portfolioApp/urls.py
+++ portfolioApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import indexPageView, ownerPageView, propertyPageView
 
 urlpatterns = [
-    path('', indexPageView, name = "index"),
+    path('services/', indexPageView, name = "index"),
     path('<uuid:person_uuid>/', ownerPageView, name='owner'),
     path('<uuid:person_uuid>/<str:property_name>/', propertyPageView, name='property'),    
 ]
