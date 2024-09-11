4d267a728904927de473ef6d02bd666856eb37ba
ec134866 <evan@thecarters.com>
2024-09-11 11:42:05 -0400

    fixing js, removing unnecessary pages urls views, scrapping base formats

--- portfolioApp/urls.py
+++ portfolioApp/urls.py
@@ -1,19 +1,8 @@
 from django.urls import path
-from .views import indexPageView,  sideprojectPageView, loginPageView, sunrisePageView, ownerPageView, propertyPageView, threeDexteriorPageView, threeDinteriorPageView, overflightPageView
+from .views import indexPageView, ownerPageView, propertyPageView
 
 urlpatterns = [
     path('', indexPageView, name = "index"),
     path('<uuid:person_uuid>/', ownerPageView, name='owner'),
-    path('<uuid:person_uuid>/<str:property_name>/', propertyPageView, name='property'),
-
-    # rid of the following not necessary
-    path('sideprojects/', sideprojectPageView, name = "sideprojects"),
-    path('login/', loginPageView, name='login'),
-    path('sunrise/', sunrisePageView, name='sunrise-model'),
-
-    # rid of the following when property is correct
-    path('overflight/', overflightPageView, name='overflight'),
-    path('interior/', threeDinteriorPageView, name='interior'),
-    path('exterior/', threeDexteriorPageView, name='exterior')
-    
+    path('<uuid:person_uuid>/<str:property_name>/', propertyPageView, name='property'),    
 ]
