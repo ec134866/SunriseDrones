0c227bfa9c4c080c5b52667967832866615eadd1
ec134866 <evan@thecarters.com>
2024-09-13 09:45:58 -0400

    potentially fixing static

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/home/SunriseDrones/SunriseDrones/static/"
+STATIC_URL = "/static/"
 # STATICFILE_DIRS = [os.path.join(BASE_DIR, "static"),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
