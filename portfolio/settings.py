b4c9b53d563c675eadf805ec1ea6ddd7955abc3d
ec134866 <evan@thecarters.com>
2024-11-04 10:49:46 -0500

    add allowed host 3

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (DJANGO_ENV == 'dev')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'sunrisedrones.pythonanywhere.com', 'balancedrockimaging.pythonanywhere.com', 'www.balancedrockimaging.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'sunrisedrones.pythonanywhere.com', 'balancedrockimaging.pythonanywhere.com', 'balancedrockimaging.com', 'www.balancedrockimaging.com']
 
 
 # Application definition
